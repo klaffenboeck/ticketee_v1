a94f91674e43b614ef215707e0aa3959000fc7c8
Manfred Klaffenboeck <mk@tailored-apps.com>
2011-10-07 16:20:45 +0200

    Ran the cucumber and rspec generators

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'f73e835c4dc5877df37219157ef52be378244436c541cf860e20ae397e1f1c95d5ce313948190cc94567a5f15122b55ebf5819e55f273135a1100f0d94e93bb7'
